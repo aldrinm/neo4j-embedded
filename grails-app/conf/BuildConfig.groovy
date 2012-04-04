0dcdced71636c6f0dd2d9ce86528bb30ceb46eca
Aldrin Misquitta <aldrinm@gmail.com>
2012-04-04 21:08:15 +0530

    Updated dependencies after commit

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,19 +30,27 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://m2.neo4j.org/content/repositories/everything/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.16'
+//        compile(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1")
+//        runtime(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1", branch:"static-web")
+//        runtime('com.sun.jersey:jersey-bundle:1.9')
+
+
         compile(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1")
-        runtime(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1", branch:"static-web")
-        runtime('com.sun.jersey:jersey-bundle:1.9')
+        runtime(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1",
+                branch:"static-web")
+        runtime('com.sun.jersey:jersey-server:1.9')
+        runtime('com.sun.jersey:jersey-core:1.9')
 
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+        //runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
 
